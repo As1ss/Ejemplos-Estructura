53a4dcb051add569d143be428e15beef353cbbfb
As1ss <alexis.lopbri@educa.jcyl.es>
2023-08-14 23:37:43 +0200

    Trabajando con views, agregado de un Label personalizado con un evento de mostrar un Toast con un mensaje personalizado. Todo ello trabajando desde el documento xml e instanciando un objeto de dicho Label

--- app/src/main/java/com/example/empty_proyect/MainActivity.kt
+++ app/src/main/java/com/example/empty_proyect/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.empty_proyect
 
+import android.graphics.Color
+import android.graphics.Typeface
+import android.media.MediaActionSound
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import java.lang.reflect.Type
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mediaPlayer = MediaPlayer.create(this,R.raw.sound)
+
+        val ejemploLabel:TextView = findViewById(R.id.ejemploLabel)
+        var contador : Int = 1
+
+        ejemploLabel.apply {
+            this.text = "Pulsa aquí"
+            setTextColor(Color.YELLOW)
+            setBackgroundColor(Color.BLACK)
+            setTypeface(Typeface.MONOSPACE,Typeface.BOLD)
+            setTextSize(3,35f)
+            textAlignment = View.TEXT_ALIGNMENT_CENTER
+            this.setTextColor(Color.YELLOW)
+        }
+        ejemploLabel.setOnClickListener{
+            mediaPlayer.start()
+            Toast.makeText(this,"Has eurptadolf ${contador++} veces.",Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
