6861ac74035e71adea11caebd99aa9fd3266496b
As1ss <alexis.lopbri@educa.jcyl.es>
2023-08-19 02:38:15 +0200

    Creación personalización y añadido de eventos mediante código sobre los objetos Chip y ChipGroups

--- app/src/main/java/com/example/empty_proyect/Buttons.kt
+++ app/src/main/java/com/example/empty_proyect/Buttons.kt
@@ -1,4 +1,13 @@
 package com.example.empty_proyect
 
-class Buttons {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Buttons : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buttons)
+
+            }
 }
